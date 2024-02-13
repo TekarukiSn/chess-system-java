b962fbc51de84008d90ac88b981ce2fa17f112a4
Davi Naves <nvsdavicz@gmail.com>
2024-02-13 16:15:06 -0300

    Moving pieces

--- src/app/UI.java
+++ src/app/UI.java
@@ -1,6 +1,10 @@
 package app;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 import chess.ChessPiece;
+import chess.ChessPosition;
 import chess.Color;
 
 
@@ -23,6 +27,17 @@ public class UI {
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+    public static ChessPosition readChessPosition(Scanner sc){
+        try{
+        String s = sc.nextLine();
+        char column = s.charAt(0);
+        int row = Integer.parseInt(s.substring(1));
+        return new ChessPosition(column, row);
+        }
+        catch(RuntimeException e){
+            throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8");
+        }
+    }
     public static void printBoard(ChessPiece[][] pieces){
         for(int i = 0; i < pieces.length; i++){
             System.out.print((8 - i) + " ");
@@ -31,7 +46,7 @@ public class UI {
             }
             System.out.println();
         }
-        System.out.println("  a b c d e f g h");  
+        System.out.println("   a b c d e f g h");  
     }
 
     private static void printPiece(ChessPiece piece) {
@@ -47,6 +62,6 @@ public class UI {
                 System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
             }
         }
-        System.out.print(" ");
+        //System.out.print(" ");
 	}
 }
