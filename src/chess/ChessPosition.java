7d48ed3601c5f897a6394ae1563e513ddda74344
Davi Naves <nvsdavicz@gmail.com>
2024-02-15 12:01:13 -0300

    check logic

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -26,7 +26,7 @@ public class ChessPosition {
         return new Position(8 - row, column - 'a');
     }
     protected static ChessPosition fromPosition(Position position){
-        return new ChessPosition((char)('a' - position.getColumn()), position.getRow() - 8);
+        return new ChessPosition((char)('a' + position.getColumn()), 8 - position.getRow());
     }
 
     @Override
